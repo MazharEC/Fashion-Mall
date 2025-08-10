1f7d734596f73e89aa6756411e61766506fe5775
Mazhar <mazhartuhin8291@gmail.com>
2025-08-10 06:01:08 +0600

    Auth Screen & Firebase

--- app/src/main/java/com/appsv/fashionmall/presentation/LoginScreen.kt
+++ app/src/main/java/com/appsv/fashionmall/presentation/LoginScreen.kt
@@ -2,6 +2,7 @@ package com.appsv.fashionmall.presentation
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,8 +15,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -35,32 +34,28 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.appsv.fashionmall.R
 import com.appsv.fashionmall.presentation.utils.CustomTextField
 
+@Preview(showSystemUi = true)
 @Composable
-fun SignUpScreen() {
+fun LoginScreenUi(){
 
     val context = LocalContext.current
-    var firstName by remember { mutableStateOf("") }
-    var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
-    var phoneNumber by remember { mutableStateOf("") }
-
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
-        ) {
         Text(
-            text = "Sign Up",
+            text = "Login",
             fontSize = 24.sp,
             style = TextStyle(fontWeight = FontWeight.Bold),
             modifier = Modifier
@@ -69,40 +64,17 @@ fun SignUpScreen() {
         )
 
         CustomTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = "First Name",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            singleLine = true,
-            leadingIcon = Icons.Default.Person,
-
-            )
-
-        CustomTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = "Last Name",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            singleLine = true,
-            leadingIcon = Icons.Default.Person,
-
-            )
-
-        CustomTextField(
             value = email,
             onValueChange = { email = it },
             label = "Email",
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 6.dp),
+                .padding(vertical = 8.dp),
             singleLine = true,
             leadingIcon = Icons.Default.Email,
 
             )
+        Spacer(modifier = Modifier.height(8.dp))
 
         CustomTextField(
             value = password,
@@ -110,50 +82,26 @@ fun SignUpScreen() {
             label = "Password",
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 6.dp),
+                .padding(vertical = 8.dp),
             visualTransformation = PasswordVisualTransformation(),
             singleLine = true,
             leadingIcon = Icons.Default.Lock,
 
             )
 
-        CustomTextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = "Confirm Password",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            visualTransformation = PasswordVisualTransformation(),
-            singleLine = true,
-            leadingIcon = Icons.Default.Lock,
 
-            )
+        Text(
+            text = "Forget Password?",
+            modifier = Modifier.align(Alignment.End)
+        )
 
-        CustomTextField(
-            value = phoneNumber,
-            onValueChange = { phoneNumber = it },
-            label = "Phone Number",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            singleLine = true,
-            leadingIcon = Icons.Default.Phone,
-
-            )
-
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = {
-                if(firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && phoneNumber.isNotEmpty()){
-
-                    if(password == confirmPassword){
-                        Toast.makeText(context, "Sign Up Successful", Toast.LENGTH_SHORT).show()
-                    }
-                    else{
-                        Toast.makeText(context, "Password does not match", Toast.LENGTH_SHORT).show()
-                    }
+                if(email.isNotBlank() && password.isNotBlank()){
+                    //verify the user
+                    Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
                 }
                 else{
                     Toast.makeText(context, "Please fill all the fields", Toast.LENGTH_SHORT).show()
@@ -162,10 +110,11 @@ fun SignUpScreen() {
             modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
             shape = RoundedCornerShape(8.dp),
             colors = ButtonDefaults.buttonColors(
-                colorResource(id = R.color.button_color)),
+                colorResource(id = R.color.button_color)
+            ),
         ){
             Text(
-                text = "Sign Up",
+                text = "Login",
                 style = TextStyle(
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
@@ -174,15 +123,16 @@ fun SignUpScreen() {
             )
         }
 
+
         Row(verticalAlignment = Alignment.CenterVertically){
 
-            Text(text = "Already have an account?")
+            Text(text = "Don't have an account?")
 
             TextButton(
-                onClick = { /*navigate to login screen*/ },
+                onClick = { /*navigate to SignUp screen*/ },
 
-            ) {
-                Text(text = "Login", style = TextStyle(color = colorResource(id = R.color.button_color)))
+                ) {
+                Text(text = "SignUp", style = TextStyle(color = colorResource(id = R.color.button_color)))
             }
 
         }
@@ -211,6 +161,6 @@ fun SignUpScreen() {
             Text(text = "Login  with Google")
         }
 
-    }
 
+    }
 }
