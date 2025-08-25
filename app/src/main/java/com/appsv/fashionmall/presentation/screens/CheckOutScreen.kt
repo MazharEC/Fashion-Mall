9cdcbf2d3b4ab0229c5c5294c9cf8852b071099a
Mazhar <mazhartuhin8291@gmail.com>
2025-08-26 03:53:40 +0600

    FrontEnd

--- app/src/main/java/com/appsv/fashionmall/presentation/screens/CheckOutScreen.kt
+++ app/src/main/java/com/appsv/fashionmall/presentation/screens/CheckOutScreen.kt
@@ -1,15 +1,29 @@
 package com.appsv.fashionmall.presentation.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.appsv.fashionmall.presentation.viewmodel.FashionMallViewModel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CheckoutScreen(
     viewModel: FashionMallViewModel = hiltViewModel(),
@@ -24,7 +38,8 @@ fun CheckoutScreen(
     val firstName = remember { mutableStateOf("") }
     val lastName = remember { mutableStateOf("") }
     val address = remember { mutableStateOf("") }
-    val zipCode = remember { mutableStateOf("") }
+    val city = remember { mutableStateOf("") }
+    val postalCode = remember { mutableStateOf("") }
     val phoneNumber = remember { mutableStateOf("") }
     val selectedMethod = remember { mutableStateOf("Standard FREE delivery over $50") }
 
@@ -33,9 +48,29 @@ fun CheckoutScreen(
         viewModel.getProductById(productId)
     }
 
-    Scaffold(
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Shipping") },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
 
-    ){innerPadding ->
 
+            when {
+                state.value.isLoading -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
     }
 }
